79c9f2ba29ec35e57a0cb6b9df321a4f885972dd
abhishek.karia <abhishek.karia@scenera.net>
2023-03-25 17:55:19 +0530

    env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+const authServerUrl = "http://192.168.0.103:8080"
+const  resourceServerUrl = "http://192.168.0.103:8090"
+
+// angular app url
+const redirectUrl = "http://192.168.0.104:4200"
+
 export const environment = {
-  production: true
+  production: true,
+  authServerUrl:authServerUrl,
+  resourceServerUrl : resourceServerUrl,
+  demoUrl : resourceServerUrl + '/api/v1/users',
+  redirectUrl : redirectUrl + '/authorized'
 };
