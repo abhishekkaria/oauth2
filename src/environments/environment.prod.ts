9ff8c41a52069c80566fdac86e708ba28d73c9b4
abhishek.karia <abhishek.karia@scenera.net>
2023-03-26 14:50:25 +0530

    env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 const authServerUrl = "http://65.0.26.70:8080"
-const  resourceServerUrl = "http://43.205.145.6:8090"
+const  resourceServerUrl = "http://52.66.197.26:8090"
 
 // angular app url
 const redirectUrl = "http://oauth2-web.s3-website.ap-south-1.amazonaws.com"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 const authServerUrl = "http://65.0.26.70:8080"
-const  resourceServerUrl = "http://43.205.145.6:8090"
+const  resourceServerUrl = "http://52.66.197.26:8090"
 
 // angular app url
 const redirectUrl = "http://oauth2-web.s3-website.ap-south-1.amazonaws.com"
