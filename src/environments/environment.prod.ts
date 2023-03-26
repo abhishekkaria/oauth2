c37f47a67bdd368ccefe5821199eebed5094d167
abhishek.karia <abhishek.karia@scenera.net>
2023-03-26 13:18:56 +0530

    env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 const authServerUrl = "http://13.233.197.217:8080"
-const  resourceServerUrl = "http://192.168.0.103:8090"
+const  resourceServerUrl = "http://15.206.145.162:8090"
 
 // angular app url
 const redirectUrl = "http://oauth2-web.s3-website.ap-south-1.amazonaws.com"
